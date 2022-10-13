5d398bfcce6b2db4602bc3eb91ed05041aff82ee
Skc4real <sundaykelechi2000@gmail.com>
2022-10-13 16:09:23 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main() {
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
-	puts("Programming is like building a multilingual puzzle\n");
+int main(void)
+
+{  
+
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return 0;
 }
