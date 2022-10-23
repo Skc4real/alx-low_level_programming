47bce4b15035b9cba4129d74e93d1d00f0214c0c
Skc4real <sundaykelechi2000@gmail.com>
2022-10-23 12:03:09 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,11 @@
 #include "main.h"
-
 /**
-* main - Prints Holberton as a message.
-* Return: Always 0 (Success)
-*/
-
-int main(void)
+* _isupper - checks for upper case character
+* @c: the character to check for
+* Description: checks for upper character.
+* Return: 1 if c is upper case. 0 otherwise
+**/
+	int _isupper(int c)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-{
-	_putchar(str[count]);
-}
-	_putchar('\n');
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
