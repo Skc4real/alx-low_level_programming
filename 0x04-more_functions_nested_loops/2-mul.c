ee886bd1969aee6a01c93ef94f9e25d1ff17a45b
Skc4real <sundaykelechi2000@gmail.com>
2022-10-23 23:33:25 -0700

    task 2 update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,5 +1,5 @@
 #include "main.h"
-	
+
 /**
 * mul - multiplies integers and returns the result
 * @a: int to be multiplied by b
@@ -9,8 +9,7 @@
 	int mul(int a, int b)
 {
 	int d;
-				
+
 	d = a * b;
-					
 	return (d);
 }
