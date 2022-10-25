6a5138ffc3ce938d1b86be420234fa7ca4fefb98
Skc4real <sundaykelechi2000@gmail.com>
2022-10-24 23:51:25 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - returns the lenght of string
+*@s : Pointer to the variable holding the string
+* Return: void
+*/
+
+int _strlen(char *s)
+{
+	int lenght;
+
+	lenght = _strlen(*s);
+}
