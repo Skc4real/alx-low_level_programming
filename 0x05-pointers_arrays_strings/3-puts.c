0beafb35602a8699980129d7e851d0ce83eac768
Skc4real <sundaykelechi2000@gmail.com>
2022-10-25 00:52:41 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - Prints a strint followed by a new line
+* @str : Pointer that hold the string of the variable
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	char str[100];
+
+	_puts("%s\n", str)
+}
