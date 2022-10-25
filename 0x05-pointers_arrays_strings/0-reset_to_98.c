082d3dd3f02018b52e13219e27ef7a3a08ee305e
Skc4real <sundaykelechi2000@gmail.com>
2022-10-24 23:07:48 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   * Pointing to a parameter int
-  * Print value of int
+  * Print value of int n
   * Return: void
   */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	*n = 98;
+	*n = 98;	
 }
