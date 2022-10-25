17ecd7abd4f6f1b0c6c53c7da08d22710b3db9ca
Skc4real <sundaykelechi2000@gmail.com>
2022-10-24 22:45:02 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * Pointing to a parameter int
+  * Print value of int
+  * Return: void
+  */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my REDAME file on C programming Task 0x05
