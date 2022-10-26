48a6badb4ea3a564c43238c839c8928060f2a13a
Skc4real <sundaykelechi2000@gmail.com>
2022-10-26 03:08:37 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_strcat - a function that concatinate 2 strings.
+*@dest: first string.
+*@src: second string.
+*Return: string.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+	a++;
+
+	while (src[b] != '\0')
+{
+	dest[a] = src[b];
+	a++;
+	b++;
+}
+	dest[a] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a README file for AlX Task 0x06-pointers, arrays and strings
