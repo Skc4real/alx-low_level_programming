756a3189840523d4ad6b4679551162ff0569a23b
Skc4real <sundaykelechi2000@gmail.com>
2022-12-01 14:21:33 -0800

    task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
-* get_endianness - check the endianness
+* get_endianness - return the endianness of the machine
 * Return: 0 for big endian, 1 for little endian
 */
 
 int get_endianness(void)
 {
 	int n = 1;
-
 	return (*((char *) &n) + '0');
 }
