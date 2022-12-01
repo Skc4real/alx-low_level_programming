44669f0c1ae08e94abb47d205906d95cf72f8446
Skc4real <sundaykelechi2000@gmail.com>
2022-12-01 15:21:05 -0800

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,26 @@
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
-* init_dog - Entry point
-* @d: dog
-* @name: name of dog
-* @age: age of dog
-* @owner: owner of dog
+* struct dog - a dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
 */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+typedef struct dog
 {
-	if (d)
-{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
-}
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+typedef dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+int _putchar(char c);
+
+#endif
